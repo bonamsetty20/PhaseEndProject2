15a5c96e9e80acc52aac1f3304523c2723417527
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-08-03 15:37:54 +0530

    first commit

---
+++ TeacherRecord.cs
@@ -0,0 +1,61 @@
+﻿using PhaseEndProject2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhaseEndProject2
+{
+    internal class TeacherDataProcessor
+    {
+        private const string FilePath = @"C:\Users\bonam\OneDrive\Desktop\Basicapp\PracticeProjects\PhaseEndProject2\PhaseEndProject2\Teacher.cs";
+
+        public static void SaveTeacherData(List<Teacher> teachers)
+        {
+            using (StreamWriter writer = new StreamWriter(FilePath))
+            {
+                foreach (Teacher teacher in teachers)
+                {
+                    writer.WriteLine($"{teacher.ID},{teacher.Name},{teacher.Class},{teacher.Section}");
+                }
+            }
+        }
+
+        public static List<Teacher> LoadTeacherData()
+        {
+            List<Teacher> teachers = new List<Teacher>();
+
+            if (File.Exists(FilePath))
+            {
+                using (StreamReader reader = new StreamReader(FilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts.Length == 4)
+                        {
+                            int id = int.Parse(parts[0]);
+                            string name = parts[1];
+                            string teacherClass = parts[2];
+                            string section = parts[3];
+
+                            teachers.Add(new Teacher
+                            {
+                                ID = id,
+                                Name = name,
+                                Class = teacherClass,
+                                Section = section
+                            });
+                        }
+                    }
+                }
+            }
+
+            return teachers;
+        }
+
+    }
+}
